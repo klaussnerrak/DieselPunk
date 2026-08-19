955f7af22173157981c853a0df7cc67eb4cb5138
klaussnerrak <rodrigoklaussner@gmail.com>
2026-08-19 18:05:20 -0300

    feat: updated credits panel, added StartScene and HudScene

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,20 +7,25 @@ public class Menu : MonoBehaviour
     [SerializeField] private Button startButton;
     [SerializeField] private Button exitButton;
     [SerializeField] private Button optionsButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button tutorialButton;
     
-    //[SerializeField] private  GameObject CreditsPanel;
+    [SerializeField] private  GameObject CreditsPanel;
     [SerializeField] private  GameObject OptionsPanel;
     [SerializeField] private  GameObject MenuPanel;
+    [SerializeField] private  GameObject TutorialPanel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startButton.onClick.AddListener(StartGame);
         exitButton.onClick.AddListener(ExitGame);
         optionsButton.onClick.AddListener(OptionsScreen);
-        //creditsButton.onClick.AddListener(CreditsScreen);
-       // CreditsPanel.SetActive(false);
+        creditsButton.onClick.AddListener(CreditsScreen);
+        tutorialButton.onClick.AddListener(TutorialScreen);
+        CreditsPanel.SetActive(false);
         OptionsPanel.SetActive(false);
         MenuPanel.SetActive(true);
+        TutorialPanel.SetActive(false);
         AudioManager.instance.PlayMusic("MenuTrainSound");
         
 
@@ -28,30 +33,42 @@ public class Menu : MonoBehaviour
 
     public void StartGame()
     {
-        //SceneManager.LoadScene("Introducao");
-        //CreditsPanel.SetActive(false);
-        //AudioManager.instance.PlayMusic("MenuTrainSound");
+        SceneManager.LoadScene("Scenes/StartScene");        
+        AudioManager.instance.PlayMusic("MenuTrainSound");
         
     }
     public void OptionsScreen()
     {
         OptionsPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        TutorialPanel.SetActive(false);
     }
-    /*public void CreditsScreen()
+
+    public void CreditsScreen()
     {
         CreditsPanel.SetActive(true);
-    }*/
+        OptionsPanel.SetActive(false);
+    }
+
+    public void TutorialScreen()
+    {
+        TutorialPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+    }
+
     public void ExitGame()
     {
         Application.Quit();
+        Debug.Log("Quit");
     }
 
     public void BackToMenu()
     {
-        //CreditsPanel.SetActive(false);
-        OptionsPanel.SetActive(false);
-        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        OptionsPanel.SetActive(false);        
     }
+
+    
 
     
 }
