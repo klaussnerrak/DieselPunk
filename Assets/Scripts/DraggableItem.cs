abf4270913dc8603974b162ac7fa3b5322189eed
Matheus Silvério <mattdavm@hotmail.com>
2026-09-11 23:06:05 -0300

    fix drag and drop + snap to grid (tilemap) logic

--- Assets/Scripts/DraggableItem.cs
+++ Assets/Scripts/DraggableItem.cs
@@ -1,50 +1,124 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
 
-public class DraggableItem : MonoBehaviour,
-    IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DraggableItem : MonoBehaviour
 {
-    // [SerializeField] private MapManager board;
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Vector3 startPosition;
+    private Grid targetGrid;
+    private Tilemap tilemap;
 
-    private Vector3 previousPosition;
-    private CanvasGroup canvasGroup;
-
-    private void Awake()
+    void Start()
     {
-        canvasGroup = GetComponent<CanvasGroup>();
+        targetGrid = FindFirstObjectByType<Grid>();
 
-        if (GetComponent<Collider2D>() == null && GetComponent<SpriteRenderer>() != null)
-            gameObject.AddComponent<BoxCollider2D>();
-    }
-
-    private void Update()
-    {
-        if (!Input.GetMouseButtonDown(1))
+        if (tilemap == null)
         {
-            Debug.Log("I will return");
-            return;
+            tilemap = FindFirstObjectByType<Tilemap>();
         }
 
-        Camera camera = Camera.main;
-        Collider2D collider = GetComponent<Collider2D>();
-        if (camera == null || collider == null)
-            return;
+        targetGrid = FindFirstObjectByType<Grid>();
 
-        Vector3 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
-        // if (collider.OverlapPoint(mousePosition))
-        //     GetComponent<TrackPiece>()?.RotatePiece();
+        GameObject tilemapObject = GameObject.Find("Tilemap");
+
+        if (tilemapObject != null)
+        {
+            tilemap = tilemapObject.GetComponent<Tilemap>();
+        }
+        else
+        {
+            Debug.LogError("Tilemap not found");
+        }
     }
 
-    public void OnBeginDrag(PointerEventData eventData)
+    void Update()
     {
+        if (isDragging)
+        {
+            Vector3 mousePosition = GetMouseWorldPosition();
+            transform.position = mousePosition + offset;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RotatePiece();
+        }
     }
 
-    public void OnDrag(PointerEventData eventData)
+    public void RotatePiece()
     {
+        transform.Rotate(0f, 0f, 90f);
     }
 
-    public void OnEndDrag(PointerEventData eventData)
+
+    private void OnMouseDown()
     {
+        isDragging = true;
+        startPosition = transform.position;
+        offset = transform.position - GetMouseWorldPosition();
     }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+        SnapTileToGrid();
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void SnapTileToGrid()
+    {
+        if (targetGrid == null || tilemap == null) return;
+
+        Vector3Int cellPosition = targetGrid.WorldToCell(transform.position);
+
+        if (IsValidPlace(cellPosition))
+        {
+            Vector3 snappedPosition = targetGrid.CellToWorld(cellPosition);
+
+            snappedPosition.x += targetGrid.cellSize.x / 2;
+            snappedPosition.y += targetGrid.cellSize.y / 2;
+            snappedPosition.z = transform.position.z;
+
+            transform.position = snappedPosition;
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
+    }
+
+    private bool IsValidPlace(Vector3Int cellPosition)
+    {
+        if (!tilemap.HasTile(cellPosition))
+        {
+            return false;
+        }
+
+        Vector3 cellWorldPos = targetGrid.CellToWorld(cellPosition);
+        cellWorldPos.x += targetGrid.cellSize.x / 2;
+        cellWorldPos.y += targetGrid.cellSize.y / 2;
+
+        Vector2 checkSize = new Vector2(targetGrid.cellSize.x * 0.8f, targetGrid.cellSize.y * 0.8f);
+        Collider2D hit = Physics2D.OverlapBox(cellWorldPos, checkSize, 0f);
+
+        if (hit != null && hit.gameObject != gameObject)
+        {
+            Debug.Log("this place is already in use");
+            return false;
+        }
+
+        return true;
+    }
+
+
+
 
 }
